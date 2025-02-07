f1f01e5440118984ad93124c9ede4ea275cd967b
k023g0096@g.neec.ac.jp <k023g0096@g.neec.ac.jp>
2025-02-07 11:54:00 +0900

    test

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,50 +1,71 @@
-#include "Particle.hlsli"
+#include "object3d.hlsli"
 
 struct Material
 {
     float32_t4 color;
-    int32_t enableLighting;
-    float32_t4x4 uvTransform;
+    int32_t enableLightng;
+    float32_t shininess;
 };
 
-struct DirectionalLight
-{
-    float32_t4 color;
-    float32_t3 direction;
-    float intensity;
-};
-
-ConstantBuffer<Material> gMaterial : register(b0);
-ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
-
-Texture2D<float32_t4> gTexture : register(t0);
-
-SamplerState gSampler : register(s0);
-
-
-
-struct PixelShaderOutput
+struct PixcelShaderOutput
 {
     float32_t4 color : SV_Target0;
 };
 
-PixelShaderOutput main(VertexShaderOutput input)
+struct DirectrionaLight
 {
-    float4 transformdUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
-    float32_t4 textureColor = gTexture.Sample(gSampler, transformdUV.xy);
+    float32_t4 color; //!< ライトの色
+    float32_t3 direction; //!< ライトの向き
+    float intensity;
+};
+
+struct Camera
+{
+    float32_t3 worldPosition;
+};
+
+ConstantBuffer<Material> gMaterial : register(b0);
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+ConstantBuffer<DirectrionaLight> gDirectrionaLight : register(b1);
+ConstantBuffer<Camera> gCamera : register(b2);
+
+PixcelShaderOutput main(VertexShaderOutput input)
+{
+    PixcelShaderOutput output;
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     
-    PixelShaderOutput output;
-    
-    if (gMaterial.enableLighting != 0)
+    if (gMaterial.enableLightng != 0)//Litingする場合
     {
-        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+
+        float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
+        
+        float32_t3 reflectLight = reflect(-gDirectrionaLight.direction, normalize(input.normal));
+
+        float RdotE = dot(reflectLight, toEye);
+        float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度  
+        float NdotL = dot(normalize(input.normal), -gDirectrionaLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
-    }
+
+       // output.color = gMaterial.color * textureColor * gDirectrionaLight.color * cos * gDirectrionaLight.intensity;
+   
+        // 拡散反射
+        float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectrionaLight.color.rgb * cos * gDirectrionaLight.intensity;
+        
+        // 鏡面反射
+        float32_t3 specular =gDirectrionaLight.color.rgb * gDirectrionaLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
+        
+        //拡散反射・鏡面反射
+        output.color.rgb = diffuse + specular;
+        //アルファは今まで通り
+        output.color.a = gMaterial.color.a * textureColor.a;
+   
+   }
     else
     {
         output.color = gMaterial.color * textureColor;
     }
-
+    
+    
     return output;
 }
--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,25 +1,28 @@
-#include "Particle.hlsli"
+#include "object3d.hlsli"
 
 struct TransformationMatrix
 {
     float32_t4x4 WVP;
     float32_t4x4 World;
 };
-StructuredBuffer<TransformationMatrix> gTranfsformationMatrices : register(t0);
+ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
-
-struct VertexShaderInput
+struct VertexShederInput
 {
     float32_t4 position : POSITION0;
-    float32_t2 texcoord : TEXCOORD0;
+    float32_t2 teccoord : TEXCOORD0;
     float32_t3 normal : NORMAL0;
+
 };
 
-VertexShaderOutput main(VertexShaderInput input,uint32_t instanceId:SV_InstanceID)
+VertexShaderOutput main(VertexShederInput input)
 {
     VertexShaderOutput output;
-    output.position = mul(input.position, gTranfsformationMatrices[instanceId].WVP);
-    output.texcoord = input.texcoord;
-    output.normal = normalize(mul(input.normal, (float32_t3x3) gTranfsformationMatrices[instanceId].World));
-    return output;
+    output.position = mul(input.position, gTransformationMatrix.WVP);
+    output.texcoord = input.teccoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
+    
+    output.worldPosition = mul(input.position, gTransformationMatrix.World).xyz;
+   
+   return output;
 }
